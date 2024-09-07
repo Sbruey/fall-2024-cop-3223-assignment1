71b44975aff68739f062862e633d680130a74b3d
Sbruey <sh504765@ucf.edu>
2024-09-07 18:23:30 +0000

    test

--- fracturing.c
+++ fracturing.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h> 
 
-int main() 
+int main(int argc, char **argv)
 {
       double calculateDistance();
       double perimeter();
@@ -14,9 +14,9 @@ int main()
       int y1 = 0;
       int y2 = 0; 
 
-      printf("Enter your points: /n");
+      printf("Enter your points: ");
 
-      printf("x1: ");
+      printf("\nx1: ");
       scanf("%d", &x1);
 
       printf("x2: ");
@@ -29,5 +29,19 @@ int main()
       scanf("%d", &y2);
 
       return 0;
+
+      double calculateDistance();
+    {
+      double distance = sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+
+      printf("The distance between the two points is %lf", distance);
+
+      printf("/nPoint #1 entered:x1 = %d, y1 = %d", x1, y1); 
+      printf("Point #2 entered:x2 = %d, y2 = %d", x2, y2); 
+
+      return 3; 
+    }
 }
 
+
+      
